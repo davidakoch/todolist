a011cb3bae3585044e54cfcfffda1ce45eece97b
David A Koch <kodave1@gmail.com>
2016-03-17 23:03:28 -0400

    added more comments to javascript file

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 //Add a new task
+	//when the button is pressed
+	//create a new list item with the text from #new-task:
+		//create an input with a checkbox
 
 //edit an existing task
 
+//delete an existing task
 
+//mark a tast as complete
+
+//mark a task as incomplete
