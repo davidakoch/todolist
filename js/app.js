e8beb9ce66a597c8584eda4858b63c95e0584aaa
David A Koch <kodave1@gmail.com>
2016-03-18 21:47:25 -0400

    added more comments to the javascript file

--- js/app.js
+++ js/app.js
@@ -1,12 +1,35 @@
 //Add a new task
 	//when the button is pressed
-	//create a new list item with the text from #new-task:
+	//create a new list item in todo with the text from #new-task:
 		//create an input with a checkbox
+		//create a label
+		//create an input when we edit the task
+		//creaete a button with the class of edit
+		//create a button with the class of delete
+		//each of these elements need to modified and appended
+			//the append method inserts specified content at the end of the selected elements
+
 
 //edit an existing task
+	//when the edit button is pressed
+		//if the class of the parent is .editMode
+			//switch from .editMode
+			//make the label text become the input's value
+		//else
+			//switch to .editMode
+			//input value becomes the label's text
+
+		//Toggle .editMode on the parent
+
 
 //delete an existing task
+	//when the delete button is pressed
+		//remove the parent list item from the unordered list
 
 //mark a tast as complete
+	//when the checkbox is checked
+		//append the task list item to the #completed-tasks
 
 //mark a task as incomplete
+	//when the checkbox is unchecked
+		//append the task list item to the #incomplete-tasks
