094bd6fda04f3ec1ffd77c7ddeed08bd723bbb3c
David A Koch <kodave1@gmail.com>
2016-03-18 21:58:57 -0400

    added var functions to javascript

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 //Add a new task
+var addTask = function() {
 	//when the button is pressed
 	//create a new list item in todo with the text from #new-task:
 		//create an input with a checkbox
@@ -8,9 +9,10 @@
 		//create a button with the class of delete
 		//each of these elements need to modified and appended
 			//the append method inserts specified content at the end of the selected elements
-
+}
 
 //edit an existing task
+var editTask = function() {
 	//when the edit button is pressed
 		//if the class of the parent is .editMode
 			//switch from .editMode
@@ -20,16 +22,25 @@
 			//input value becomes the label's text
 
 		//Toggle .editMode on the parent
-
+}
 
 //delete an existing task
+var deleteTask = function() {
 	//when the delete button is pressed
 		//remove the parent list item from the unordered list
+}
+
+
 
 //mark a tast as complete
+var taskCompleted = function() {
 	//when the checkbox is checked
 		//append the task list item to the #completed-tasks
+}
 
 //mark a task as incomplete
+var taskIncomplete = function() {
 	//when the checkbox is unchecked
 		//append the task list item to the #incomplete-tasks
+}
+
